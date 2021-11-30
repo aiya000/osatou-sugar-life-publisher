42cdd3b5f19a9f900a3b6677e450dcfd4ac1e287
aiya000 <aiya000.develop@gmail.com>
2021-11-30 19:42:42 +0900

    :sparkles: Add the title onto Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,13 +2,21 @@ import { FunctionComponent } from 'react'
 import Link from 'next/link'
 
 const Navbar: FunctionComponent = () => (
-  <ul className="flex bg-pink-300 p-4">
-    <li className="mr-6">
-      <Link href="/">
-        <a className="text-white hover:text-black fond-extrabold">Home</a>
-      </Link>
-    </li>
-  </ul>
+  <div className="flex justify-between items-center bg-pink-300 p-4">
+    <Link href="/">
+      <a className="btn">お砂糖届け出市役所</a>
+    </Link>
+
+    {/*
+      NOTE:
+      「タイトルをクリックするとHomeに戻る」
+      っていうのは案外、知らない人はいるらしいので、
+      明示的なナビゲーションも用意しておく
+    */}
+    <Link href="/">
+      <a className="btn">Home</a>
+    </Link>
+  </div>
 )
 
 export default Navbar
