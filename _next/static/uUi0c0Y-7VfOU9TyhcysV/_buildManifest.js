1777e6ab480047a9bb97f6d160c56ee386b2c332
aiya000 <aiya000@users.noreply.github.com>
2021-12-02 17:39:25 +0000

    deploy: c57bbaa1563db97ca3e5e51690daae73cb409490

---
+++ _next/static/uUi0c0Y-7VfOU9TyhcysV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-a27498432c081d1f.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/input":["static/chunks/pages/input-7952a35967bd4d78.js"],sortedPages:["/","/_app","/_error","/input"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
