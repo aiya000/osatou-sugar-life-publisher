4add0be4c708610931801e10374dbda91938fc61
aiya000 <aiya000.develop@gmail.com>
2021-12-03 06:58:44 +0900

    :sparkles: Add a burger menu onto Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,41 @@
 import Link from 'next/link'
-import { FunctionComponent } from 'react'
+import Links from '@/components/Links'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FunctionComponent, useState } from 'react'
+import { faBars, faChevronCircleRight } from '@fortawesome/free-solid-svg-icons'
 import { title } from '@/data/title'
 
-const Navbar: FunctionComponent = () => (
-  <div className="flex justify-between items-center bg-pink-300 p-4">
-    <Link href="/">
-      <a className="btn">{title}</a>
-    </Link>
+const Navbar: FunctionComponent = () => {
+  const [isMenuOpening, setIsMenuOpening] = useState(false)
 
-    <Link href="/input">
-      <a className="btn">発行する</a>
-    </Link>
-  </div>
-)
+  return (
+    <>
+      <div className="flex justify-between items-center bg-pink-300 p-4 h-16">
+        <Link href="/">
+          <a className="btn">{title}</a>
+        </Link>
+
+        {/* For mobile devices */}
+        <div className="flex justify-end w-1/2 ml-1 visible md:invisible">
+          <button onClick={() => setIsMenuOpening(!isMenuOpening)}>
+            <FontAwesomeIcon icon={faBars} width={40} className="text-pink-500" />
+          </button>
+        </div>
+
+        {/* For PC */}
+        <div className="flex justify-end ml-1 w-0 h-0 md:w-auto md:h-auto">
+          <div className="flex justify-end ml-1 invisible md:visible">{Links}</div>
+        </div>
+      </div>
+
+      {isMenuOpening && (
+        <div className="absolute flex flex-row">
+          <div className="w-1/4v">　</div>
+          <div className="w-3/4v h-screen bg-pink-100 flex flex-col">{Links}</div>
+        </div>
+      )}
+    </>
+  )
+}
 
 export default Navbar
