e196ddcd163c127e216f115e20641d90167a07ab
aiya000 <aiya000.develop@gmail.com>
2021-12-03 03:01:08 +0900

    :sparkles: Allow to route on gh-pages by url-prefix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
+const urlPrefix = process.env.NODE_ENV === 'production' ? '/osatou-sugar-life-publisher' : ''
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  handleImages: ['jpeg', 'png', 'svg', 'webp', 'gif'],
+  handleImages: ['jpeg', 'png', 'svg', 'webp', 'gif'], // To handle for a type 'StaticImageData'
+
+  /*
+   * For gh-pages.
+   */
+  trailingSlash: true,
+  assetPrefix: urlPrefix,
+  basePath: urlPrefix,
 }
