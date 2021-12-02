4a0fd0a4dfabfd9e84e51d790f8746950d90a1dd
aiya000 <aiya000@users.noreply.github.com>
2021-12-02 17:29:44 +0000

    deploy: 08cf6ca5f57532c98aae550b4371fa385994aefb

--- _next/static/AsZR7GCY3idf4CsWPUOHa/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-4a830e5c255aa38b.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
