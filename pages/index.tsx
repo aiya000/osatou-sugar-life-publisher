d6b3e4171a3f49ee9b1faa043ca3c9d71d41d32e
aiya000 <aiya000.develop@gmail.com>
2021-12-03 00:53:56 +0900

    :sparkles: Apply a style for all 'a' tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,14 +32,8 @@ const Home: NextPage = () => (
       <div className="border-t-2 border-dotted border-pink-400 w-1/2 h-2 mt-8">　</div>
 
       <p className="my-4">
-        Created by sisters{' '}
-        <a href="https://twitter.com/senka_ai_vrchat" className="link">
-          千夏あい
-        </a>{' '}
-        and{' '}
-        <a href="https://twitter.com/public_ai000ya" className="link">
-          あいや
-        </a>
+        Created by sisters <a href="https://twitter.com/senka_ai_vrchat">千夏あい</a> and{' '}
+        <a href="https://twitter.com/public_ai000ya">あいや</a>
       </p>
     </main>
   </div>
