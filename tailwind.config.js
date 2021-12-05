13bf05c1525509caf9cdbed9586a66ef495c56c9
aiya000 <aiya000.develop@gmail.com>
2021-12-06 05:00:31 +0900

    :sparkles: Add default tailwind colors explicitly
    But the reason is nothing. lol :D

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    /**
+     * Default colors.
+     */
+    transparent: 'transparent',
+    current: 'currentColor',
+    black: colors.black,
+    white: colors.white,
+    gray: colors.coolGray,
+    red: colors.red,
+    yellow: colors.amber,
+    blue: colors.blue,
   },
   variants: {
     extend: {},
